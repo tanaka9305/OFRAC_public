e2d2623746d66333ccc74890d7ebb78d6b551fb9
Kohei Tanaka <155218083+tanaka9305@users.noreply.github.com>
2025-08-13 02:37:08 +0900

    冒頭を変えやすくした

--- damper_speed/damper_speed_diff_filter.py
+++ damper_speed/damper_speed_diff_filter.py
@@ -3,14 +3,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import signal
 
-filename = 'skidpad_damper.csv'
+##CSVファイル名と時間設定（書き換える）
+filename = '20250802_OF_25Iida_003.csv'
+start_time = 30  # 秒
+end_time = 33    # 秒
+use_cols = 4
+###################################
+
 sampling_rate = 50  # サンプリングレート [Hz]
 Ts = 1 / sampling_rate  # サンプリング周期 [s]
+volt2disp = 0.075/5  # 電圧から変位への変換係数 [m/V]
+cut_off = 25  # カットオフ周波数 [Hz]
 
 def import_data():
-    start_time = 30  # 秒
-    end_time = 33    # 秒
-
     start_row = int(start_time / Ts + 17)  # データ開始行（ヘッダ込み）
     end_row = int(end_time / Ts + 17)
 
@@ -18,14 +23,14 @@ def import_data():
     df = pd.read_csv(
         filename,
         encoding='shift_jis',
-        usecols=[4],  # 必要な列（0始まりの列番号）
+        usecols=[use_cols],  # 必要な列（0始まりの列番号）
         skiprows=start_row,
         nrows=end_row - start_row
     )
     return df
 
 def diff_filter(time_series):
-    fc = 25           # カットオフ周波数 [Hz]
+    fc = cut_off  # カットオフ周波数 [Hz]
     wc = 2 * np.pi * fc  # カットオフ角周波数 [rad/s]
     alpha = 1 / (1 + wc * Ts)
 
@@ -35,10 +40,10 @@ def diff_filter(time_series):
         dx = (time_series[k] - time_series[k-1]) / Ts
         y[k] = alpha * y[k-1] + (1 - alpha) * dx
 
-    return y
+    return volt2disp * y
 
 def diff_ideal(time_series):
-    return np.gradient(time_series, Ts)
+    return np.gradient(volt2disp * time_series, Ts)
 
 def main():
     # データのインポート
@@ -54,7 +59,6 @@ def main():
 
     # プロット
     plt.figure(figsize=(10, 5))
-    plt.plot(t, data, label='Original Data')
     plt.plot(t, diff_filter_data, label='Differentiated Data', linestyle='--')
     plt.plot(t, diff_ideal_data, label='Ideal Differentiated Data', linestyle=':')
     plt.title('Damper Speed with Low-pass Filter')
